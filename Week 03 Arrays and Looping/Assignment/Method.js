2472a96a426bf8020c0659ec7a38315ec14dcec2
Muhammad Arham <67412607+Arham-12336@users.noreply.github.com>
2022-12-29 18:33:50 +0500

    Boxers method

---
+++ Week 03 Arrays and Looping/Assignment/Method.js
@@ -0,0 +1,14 @@
+// let bestBoxers=new Map();
+// bestBoxers.set(1,"The champion");
+// bestBoxers.set(2,"The runner up");
+// bestBoxers.set(3,"The last");
+// console.log(bestBoxers);
+
+
+// bestBoxers.get(2);
+
+
+nums=[0,10,20,30,40]
+map(function(nums){
+    return nums/10;
+})
